2ec3b103d39c29964a9350812025841d8d412452
zails23 <zails@yahoo.com>
2017-08-09 18:46:11 -0700

    Wednesday evening

--- final_project_template/js/main.js
+++ final_project_template/js/main.js
@@ -1,1 +1,15 @@
-// Make it rain!
+// When a user scrolls to the section My work (.work)
+  // Slowly fadeIn the class "row" (.row)
+  // Else Hide .work
+
+// When a user clicks on My Work in the nav,
+  //  Jump to My Work section (.work)
+  //  Slowly fadeIn the class "row" (.row)
+  //  Else hide .work
+
+// When a user scrolls to the section favorite things (.favethings)
+  //  Slowly fadeIn the section (.favethings)
+  //  Else hide .favethings
+
+// When a user scrolls to the contact section (.contact)
+  //  Slowly fadeIn the section .contact 
