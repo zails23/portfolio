b52a567ebe66d72afb998b93b304648ed2ca2027
zails23 <zails@yahoo.com>
2017-08-15 23:27:49 -0700

    Thru Tues Aug. 15

--- final_project_template/js/main.js
+++ final_project_template/js/main.js
@@ -1,23 +1,21 @@
 
+
 $(window).on('scroll', function () {
   var distanceScrolled = $(window).scrollTop();
   console.log(distanceScrolled);
 
-  // $('.fave-things').not(':offscreen').addClass('faded-in');
-  // $('.projects').not(':offscreen').addClass('faded-in');
-  //
-  if ($('.fave-things') )
-  // if ($(document).scrollTop() >= 650) {
-  //   $('.projects').addClass('faded-in');
-  // }
-  // if ($(document).scrollTop() >= 1) {
-  //   console.log('true');
-  //   $('.fave-things').addClass('faded-in');
-  //
-  // }
+  if ($(document).scrollTop() >= 750) {
+    $('.projects').addClass('faded-in');
+  }
+  if ($(document).scrollTop() >= 2013) {
+    $('.fave-things').addClass('faded-in');
+  }
 });
 
-// when target offset top = (value offset of the window) => then trigger fade in. 
+
+// $('.fave-things').not(':offscreen').addClass('faded-in');
+// $('.projects').not(':offscreen').addClass('faded-in');
+// // when target offset top = (value offset of the window) => then trigger fade in.
 
 // When a user scrolls to the contact section (.contact)
 //  Slowly fadeIn the section .contact
